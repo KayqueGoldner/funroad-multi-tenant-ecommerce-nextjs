74e7f22706309aea5dcb53c4bd376672aed66017
Kayque Goldner <kayquegoldner@gmail.com>
2025-04-15 18:59:08 -0300

    refactor ui components and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
 import { Button } from "@/components/ui/button";
-
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
 const Home = () => {
   return (
-    <div>
-      <Button variant="outline">Click me</Button>
-      <p className="text-lg font-bold text-blue-500 underline">Hello World</p>
+    <div className="flex w-full max-w-md flex-col gap-4 p-10">
+      <div>
+        <Button variant="elevated">Click me</Button>
+      </div>
+      <div>
+        <Input placeholder="Input" />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder="Textarea" />
+      </div>
+      <div className="flex gap-2">
+        <Checkbox />
+        <Checkbox checked />
+      </div>
     </div>
   );
 };
