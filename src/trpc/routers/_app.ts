bec6ec36bbbeb62be4bbc7946d2eebc108662731
Kayque Goldner <kayquegoldner@gmail.com>
2025-05-12 00:10:00 -0300

    checkout page

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,6 +3,7 @@ import { authRouter } from "@/modules/auth/server/procedures";
 import { productsRouter } from "@/modules/products/server/procedures";
 import { tagsRouter } from "@/modules/tags/server/procedures";
 import { tenantsRouter } from "@/modules/tenants/server/procedures";
+import { checkoutRouter } from "@/modules/checkout/server/procedures";
 
 import { baseProcedure, createTRPCRouter } from "../init";
 
@@ -12,6 +13,7 @@ export const appRouter = createTRPCRouter({
   products: productsRouter,
   tags: tagsRouter,
   tenants: tenantsRouter,
+  checkout: checkoutRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
